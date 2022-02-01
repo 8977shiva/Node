a791b4516246438a03106a1832ffae5b71a79095
8977shiva <b.skreddy1997@gmail.com>
2022-02-01 15:51:35 +0530

    node basic's

---
+++ Project-planrets/index.js
@@ -0,0 +1,32 @@
+// import { parse } from "csv-parse";
+// import fs from "fs";
+const parse = require("csv-parse");
+const fs = require("fs");
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse.parse({
+      comment: "#",
+      columns: true
+    })
+  )
+  .on("data", (data) => {
+    isHabitablePlanet(data) ? habitablePlanets.push(data) : null;
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(habitablePlanets.length);
+    console.log("done");
+  });
---
+++ built in modules/event.js
@@ -0,0 +1,12 @@
+// import EventEmitter from "events";
+
+// const celebrity = new EventEmitter();
+
+// // observing to an event with on method
+// // on(which event listening to, listener function )
+// celebrity.on("new video", () => {
+//   console.log("Like this new video");
+// });
+
+// // triggering  the event
+// celebrity.emit("new video");
